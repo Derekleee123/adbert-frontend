ec1b087e950b4b9db351e98308288bb3501f50c2
Derek Liang <f80673@gmail.com>
2025-05-08 23:24:06 +0800

    remove unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ClickCounter from "./components/ClickCounter";
 import CryptoChart from "./components/CryptoChart";
 
--- src/components/ClickCounter.tsx
+++ src/components/ClickCounter.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import { useDispatch, useSelector } from "react-redux";
